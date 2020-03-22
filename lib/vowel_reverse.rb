cc33ccbf5d2341a43d7771f8e24de09b8db512b7
AJ Tran <anh.jamietran@gmail.com>
2020-03-22 15:52:08 -0600

    Refactor ugly code

--- lib/vowel_reverse.rb
+++ lib/vowel_reverse.rb
@@ -13,34 +13,8 @@ class VowelReverse
   end
 
   def vowel_strip
-    str_arr = @str.chars
-    vow_arr = []
-
-    str_arr.each do |c|
-      if c === "A"
-        vow_arr << c
-      elsif c === "E"
-        vow_arr << c
-      elsif c === "I"
-        vow_arr << c
-      elsif c === "O"
-        vow_arr << c
-      elsif c === "U"
-        vow_arr << c
-      elsif c === "a"
-        vow_arr << c
-      elsif c === "e"
-        vow_arr << c
-      elsif c === "i"
-        vow_arr << c
-      elsif c === "o"
-        vow_arr << c
-      elsif c === "u"
-        vow_arr << c
-      end
-    end
-
-    vow_arr.join
+    vowels = @str.scan(/[aeiou]/i).join
   end
+
 
 end
