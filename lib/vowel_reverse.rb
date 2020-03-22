322d983495d3cef9931fd47bb57071205944c0af
AJ Tran <anh.jamietran@gmail.com>
2020-03-22 14:48:33 -0600

    Add ugly vowel_strip method

--- lib/vowel_reverse.rb
+++ lib/vowel_reverse.rb
@@ -12,4 +12,35 @@ class VowelReverse
     @str.reverse
   end
 
+  def vowel_strip
+    str_arr = @str.chars
+    vow_arr = []
+
+    str_arr.each do |c|
+      if c === "A"
+        vow_arr << c
+      elsif c === "E"
+        vow_arr << c
+      elsif c === "I"
+        vow_arr << c
+      elsif c === "O"
+        vow_arr << c
+      elsif c === "U"
+        vow_arr << c
+      elsif c === "a"
+        vow_arr << c
+      elsif c === "e"
+        vow_arr << c
+      elsif c === "i"
+        vow_arr << c
+      elsif c === "o"
+        vow_arr << c
+      elsif c === "u"
+        vow_arr << c
+      end
+    end
+
+    vow_arr.join
+  end
+
 end
