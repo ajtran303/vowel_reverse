7cb7cd15ba9a4e92955d007b3d834b4a26901323
AJ Tran <anh.jamietran@gmail.com>
2020-03-22 17:46:23 -0600

    Refactor code

--- lib/vowel_reverse.rb
+++ lib/vowel_reverse.rb
@@ -1,1 +1,28 @@
 # Iteration 4: delete everything and do it again!
+
+class VowelReverse
+  attr_reader :str
+
+  def initialize(str)
+    if str.is_a? String
+      @str = str
+    end
+  end
+
+  def reverse_vowels
+    star_vowels_arr = @str.gsub(/[aeiou]/i, "*").split("")
+
+    rev_vowels_arr = @str.scan(/[aeiou]/i)
+
+    n = star_vowels_arr.map do |char|
+      if char == "*"
+        char = rev_vowels_arr.pop
+      else
+        char
+      end
+    end
+
+    n.join
+  end
+
+end
--- test/vowel_reverse_test.rb
+++ test/vowel_reverse_test.rb
@@ -4,5 +4,28 @@ require_relative '../lib/vowel_reverse'
 
 class VowelReverseTest < Minitest::Test
 
+  def test_it_initializes_with_string
+    # skip
+    string = VowelReverse.new("string")
+    assert_equal "string", string.str
+  end
+
+  def test_it_returns_the_string_with_reversed_vowels
+    # skip
+    solved = VowelReverse.new("A.u.E.o.I.i.O.e.U.a")
+    assert_equal "a.U.e.O.i.I.o.E.u.A", solved.reverse_vowels
+  end
+
+  def test_it_doesnt_take_a_number
+    string = VowelReverse.new(2)
+    assert_nil string.str
+  end
+
+# my brain hurts what do i do now
+  def test_it_does_weird_stuff_with_numbers
+    skip
+    string = VowelReverse.new(2)
+    assert_equal "", string.reverse_vowels
+  end
 
 end
